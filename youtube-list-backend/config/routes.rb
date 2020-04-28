90a0ccacc0a96bf7fc26d332d600a7995e70514a
oliverfeher <oliver92.feher@gmail.com>
2020-04-27 22:50:56 -0400

    Add users routings and sessinos routing

--- youtube-list-backend/config/routes.rb
+++ youtube-list-backend/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/users/:id', to: 'users#show'
+      get '/users', to: 'users#index'
+      resource :users, only: [:update, :create]
+      post "/login", to: "sessions#new"
+      post "/logout", to: "sessions#destroy"
+    end
+  end
 end
