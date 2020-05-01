d4a87f6ecd0be3f43301b7218001603d73efadab
oliverfeher <oliver92.feher@gmail.com>
2020-05-01 05:05:44 -0400

    Build basic videos controller

---
+++ youtube-list-backend/app/controllers/api/v1/videos_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::VideosController < ApplicationController
+
+    # USER ADDS NEW VIDEO TO A PLAYLIST
+    def create
+
+    end
+
+    # USER DELETES VIDEO FROM A PLAYLIST
+    def delete
+
+    end
+
+end
