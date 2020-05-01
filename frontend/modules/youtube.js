378cae9b72dd1cfc810d4dffb58683f15115bd61
oliverfeher <oliver92.feher@gmail.com>
2020-04-30 23:42:43 -0400

    Build Youtube class
    - Add GET fetch request

--- frontend/modules/youtube.js
+++ frontend/modules/youtube.js
@@ -1,2 +1,18 @@
-const KEY = "AIzaSyDSKjl19sMQAmB5ccuTJHtk7ZvlNRdWxu0";
+class Youtube 
+{
+
+    static KEY = "AIzaSyDSKjl19sMQAmB5ccuTJHtk7ZvlNRdWxu0";
+    static baseURL = "https://www.googleapis.com/youtube/v3/search";
+
+    static get()
+    {
+        return fetch(baseURL,{
+            params: {
+                part: "snippet",
+                maxResults: 5,
+                key: KEY
+            }
+        });
+    }
+}
 
