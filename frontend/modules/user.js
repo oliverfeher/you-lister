a3aebee29633f6bc339947093a97647d580cb5d1
oliverfeher <oliver92.feher@gmail.com>
2020-05-01 00:30:17 -0400

    Finish building youtube API get request
    - Upon succes, render 5 iframe elements embedded from response

--- frontend/modules/user.js
+++ frontend/modules/user.js
@@ -13,7 +13,19 @@ class User
 
     searchVideos(event)
     {
-        Youtube.get(event.target.parentNode.children[2].value).then(data=>console.log(data))
+        function renderSearch(videoList)
+        {
+            Array.from(document.querySelectorAll("iframe")).forEach(video=>video.remove())
+            videoList.items.forEach(video=> {
+                let vid = document.createElement("iframe");
+                vid.setAttribute("src", `https://www.youtube.com/embed/${video.id.videoId}`)
+                let container = document.querySelector("#search-result");
+                container.appendChild(vid);
+            })
+        }
+
+        Youtube.get(event.target.parentNode.children[2].value).then(data=> renderSearch(data))
+        
     }
 
 }
