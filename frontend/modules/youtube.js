2c09665a07d9b7735491c7add07e575a834ed98b
oliverfeher <oliver92.feher@gmail.com>
2020-05-04 13:12:11 -0400

    api

--- frontend/modules/youtube.js
+++ frontend/modules/youtube.js
@@ -1,12 +1,12 @@
 // FETCHING FROM YOUTUBE API, MAX RESULT 5
 class Youtube 
 {
-    static KEY = 
+    static KEY = YOUTUBE_API_KEY;
     static baseURL = "https://www.googleapis.com/youtube/v3/search";
 
     static get(query)
     {
-        return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&key=AIzaSyDRQsr63W-2NK-C9nTM_g3odYYY00umsww&q=${query}`).then(response=>response.json())
+        return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&key=${Youtube.KEY}&q=${query}`).then(response=>response.json())
     }
 }
 
